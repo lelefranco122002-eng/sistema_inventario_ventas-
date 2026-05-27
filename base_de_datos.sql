29c7e11195b0d775776d48b0415a86c8e0e7a26a
leslie patricia portillo franco <6051220@incb.edu.sv>
2026-05-27 11:23:01 -0600

    Documentadas consultas UPDATE y DELETE en el archivo SQL

---
+++ base_de_datos.sql
@@ -0,0 +1,7 @@
+-- CONSULTAS DE PRÁCTICA DEL MÓDULO (Guía 9)
+
+-- Actualización simultánea de precio y stock:
+-- UPDATE productos SET precio = 720.00, stock = 15 WHERE id = 1;
+
+-- Eliminación de producto descontinuado:
+-- DELETE FROM productos WHERE id = 4
