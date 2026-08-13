53c9d4977caeb330004a601304ba0d945d677722
leslie patricia portillo franco <6051220@incb.edu.sv>
2026-08-13 07:40:48 -0600

    Inicio del módulo de compras: Agregada tabla y vista de proveedores

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -76,3 +76,23 @@ SELECT c.nombre_categoria, SUM(p.stock) AS existencias_totales
 FROM productos p
 INNER JOIN categorias c ON p.categoria_id = c.id
 GROUP BY c.nombre_categoria;
+-- =========================================
+-- TABLA PROVEEDORES
+-- MÓDULO DE COMPRAS
+-- =========================================
+
+CREATE TABLE proveedores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_empresa VARCHAR(100) NOT NULL,
+    contacto VARCHAR(100),
+    telefono VARCHAR(20),
+    direccion TEXT
+);
+
+-- Proveedores de prueba
+
+INSERT INTO proveedores 
+(nombre_empresa, contacto, telefono, direccion) 
+VALUES
+('Tech Data El Salvador', 'Juan Pérez', '2255-8899', 'San Salvador, Col. Escalón'),
+('Distribuidora de Papel', 'María Gómez', '2666-4433', 'San Miguel, Centro');
